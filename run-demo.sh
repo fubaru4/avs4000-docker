baac4062428b5c6d4371aea9d74a5a4330fc8b81
Craig Zimmerman <craig.zimmerman@inno-plex.com>
2020-06-10 17:55:30 -0400

    Built and tested vms

--- run-demo.sh
+++ run-demo.sh
@@ -12,4 +12,4 @@
 # This script will start a docker container that contains the AVS4000d application inside of
 # an Ubuntu 18.04 image.
 #
-docker container run -it --rm --name avs4000 --privileged -v /dev:/dev avid/avs4000:1.0
+docker container run -it --rm --name avs4000-demo --privileged -v /dev:/dev -e DISPLAY=$DISPLAY -v /tmp/.X11-unix:/tmp/.X11-unix avid/avs4000-demo:1.0
