20ff303c2bba6f4075c24c09233e84645973e7bd
Craig Zimmerman <craig.zimmerman@inno-plex.com>
2020-06-10 15:23:06 -0400

    Forgot to save file

---
+++ run.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# Prerequisites:
+#   - You have sudo privs
+#   - The 90-usb-avs4000.rules file has been installed on the docker host
+#   - The following has been run on the docker host:
+#	- sudo udevadm control --reload-rules
+#	- sudo udevadm trigger
+#
+
+#
+# This script will start a docker container that contains the AVS4000d application inside of
+# an Ubuntu 18.04 image.
+#
+docker container run -it --rm --name avs4000 --privileged -v /dev:/dev avid/avs4000:1.0
